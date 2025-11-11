3bd2650ef05d69a06d10299dbd0c1745577e8248
Taiwo Adesina <tolajide74@gmail.com>
2025-11-11 07:37:23 +0000

    ProgramDetails

--- resources/views/website/programView.blade.php
+++ resources/views/website/programView.blade.php
@@ -6,7 +6,7 @@
             <div class="row">
                 <div class="col-12">
                     <div class="breadcrumb_box text-center">
-                        <h2 class="breadcrumb-title">Our programs</h2>
+                        <h2 class="breadcrumb-title">{{ $program->program_name ?? '' }}</h2>
                         <ul class="breadcrumb-list">
                             <li class="breadcrumb-item"><a href="./">Home</a></li>
                             <li class="breadcrumb-item active">Our programs</li>
@@ -46,7 +46,7 @@
                                 <!-- === Sidebar Widget Start === -->
                                 <div class="sidebar-widget widget-images wow move-up">
 
-                                    <img class="img-fluid" src="assets/images/blog/blog-sidebar-ads.webp" alt="">
+                                    <img class="img-fluid" src="{{ asset('elsFront/images/blog/blog-sidebar-ads.webp')}}" alt="">
 
                                 </div>
                                
@@ -57,11 +57,14 @@
                                 <div class="single-blog-item">
                                     <!-- Post Feature Start -->
                                     <div class="post-feature blog-thumbnail  wow move-up">
-                                        <img class="img-fluid" src="assets/images/blog/blog-07-770x400.webp" alt="Blog Images">
+                                        
+                                        <img class="img-fluid" src="{{ asset('program-banner/'. $program->banner )}}" alt="">
                                     </div>
                                     <div class="post-info lg-blog-post-info  wow move-up">
-                                        
                                         <h5 class="post-title">{{ $program->program_name ?? '' }}</h5>
+                                    </div>
+                                    <div class="post-excerpt mt-15" style="text-align: justify;">
+                                        <p style="text-align: justify;"> {!! $program->description !!}</p>
                                     </div>
                                 </div>
                             </div>
